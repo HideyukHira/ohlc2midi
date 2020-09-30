1da7db27e7067bef9d66d7c58d2d7e8fbb14002f
hidex <hidexallpowers@gmail.com>
2020-09-30 21:04:54 +0900

    add midi.py and import prety_midi

---
+++ midi.py
@@ -0,0 +1,14 @@
+import pretty_midi
+
+# pretty_midiオブジェクトを作ります
+pm = pretty_midi.PrettyMIDI(resolution=960, initial_tempo=120)
+#instrumentはトラックみたいなものです。
+instrument = pretty_midi.Instrument(0)
+
+note_number = pretty_midi.note_name_to_number('G4')
+#noteはNoteOnEventとNoteOffEventに相当します。
+note = pretty_midi.Note(velocity=100, pitch=note_number, start=0, end=1)
+
+instrument.notes.append(note)
+pm.instruments.append(instrument)
+pm.write('test.mid') #midiファイルを書き込みます。
